a9c76e658fbb04179a604031db6d0bdf38b3107f
Nero <david.zeballos@alumnos.ucn.cl>
2024-12-09 00:09:32 -0300

    fix: Corrección footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterModule],
+    imports: [RouterModule, HeaderComponent, FooterComponent],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
